5bb0f158c6333878d93dfb2407915f9854d545fc
Arhamdeez123 <m.arham.babar@gmail.com>
2025-01-21 04:23:51 +0000

    dataTypes

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+
+//alert(3+3) // can't use coz we are using node JS not browser
+// code readability should be good
+
+console.log(3 + 3)
+     
+let name = "Arham"
+let age = 20;
+let isLoggedin = false;
+
+// number range => 2 to the power of 52 
+// bigint
+// string => use in double quote ""
+// boolean => true/false 
+// null => standalone value 
+// undefined => not assigned
+// symbol =>unique , will be used in react 
+
+// object
+
+// to get the type of that value 
+console.log(typeof "Arham");
+
+console.log(typeof null); // Null is an object 
+
+console.log(typeof undefined); // unedfined is undefined
+
